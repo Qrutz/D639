7048378a9248b28560370cb37e803febdb119ed4
Johan Sandgren <gussanjoba@student.gu.se>
2024-04-10 10:24:25 +0200

    fix build

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     if (argc == 2)
     {
         int number = std::stoi(argv[1]);
-        PrimeChecke!!r pc;
+        PrimeChecker pc;
         std::cout << "Helsing, Alexander; " << number << " is a prime number? " << pc.isPrime(number) << std::endl;
     }
     return 0;
