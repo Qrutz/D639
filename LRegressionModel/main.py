67a62c32005e9345f3343d009a4acd2eb704f4b9
Johan Sandgren <gussanjoba@student.gu.se>
2024-05-12 18:44:33 +0200

    temp

--- LRegressionModel/main.py
+++ LRegressionModel/main.py
@@ -10,6 +10,14 @@ import joblib
 def load_and_clean(filepath, columns):
     """Load CSV file with specified delimiter and keep only necessary columns."""
     df = pd.read_csv(filepath, delimiter=";", usecols=columns)
+
+    df["timestamp"] = pd.to_datetime(
+        df["sampleTimeStamp.seconds"] * 1e6 + df["sampleTimeStamp.microseconds"],
+        unit="us",
+    )
+
+    df["timestamp"] = df["timestamp"].dt.round("1s")
+
     return df
 
 
@@ -17,7 +25,19 @@ def merge_dataframes(dataframes, merge_on):
     """Merge dataframes based on the given keys."""
     merged_df = dataframes[0]
     for df in dataframes[1:]:
-        merged_df = pd.merge(merged_df, df, on=merge_on, how="outer")
+        merged_df = pd.merge(merged_df, df, on=merge_on, how="inner")
+
+    # drop og timestamp cols
+    merged_df.drop(
+        [
+            "sampleTimeStamp.seconds_x",
+            "sampleTimeStamp.microseconds_x",
+            "sampleTimeStamp.seconds_y",
+            "sampleTimeStamp.microseconds_y",
+        ],
+        axis=1,
+        inplace=True,
+    )
     return merged_df
 
 
@@ -26,45 +46,44 @@ def train_model(combined_df):
     # Normalize data for training/model preparation
     scaler = StandardScaler()
     features_to_scale = [
-        "IRLeft_voltage",
-        "IRRight_voltage",
-        "groundSteering",
+        # "IRLeft_voltage",
+        # "IRRight_voltage",
+        # "groundSteering",
         "angularVelocityX",
         "angularVelocityY",
         "angularVelocityZ",
     ]
+
+    print(combined_df[features_to_scale])
+
     combined_df[features_to_scale] = scaler.fit_transform(
         combined_df[features_to_scale]
     )
 
-    # Set target variable
-    X = combined_df.drop(["groundSteering"], axis=1)
+    # Separate features and target variable
+    X = combined_df[features_to_scale]
     y = combined_df["groundSteering"]
 
+    # handle missing data in features
+    imputer = SimpleImputer(strategy="mean")
+    X = imputer.fit_transform(X)
+
+    # Split data into training and test sets
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
 
-    imputer = SimpleImputer(strategy="mean")
-
-    # Fit and transform the data to handle missing values
-    X_train_imputed = imputer.fit_transform(X_train)
-    X_test_imputed = imputer.transform(X_test)
-
-    # Make sure input y also has no missing values
-    y_train = y_train.fillna(y_train.mean())
-    y_test = y_test.fillna(y_test.mean())
-
-    # Train model
+    # Train the model
     model = LinearRegression()
-    model.fit(X_train_imputed, y_train)
+    model.fit(X_train, y_train)
 
-    # Evaluate model
-    y_pred = model.predict(X_test_imputed)
-    mse = mean_squared_error(y_test, y_pred)
-    print(f"Mean Squared Error: {mse}")
+    # Evaluate the model
+    predictions = model.predict(X_test)
 
-    return model, imputer
+    mse = mean_squared_error(y_test, predictions)
+    print(f"Mean squared error: {mse}")
+
+    return model, imputer, scaler
 
 
 # data[0] = "CSV-Files/REC0 /opendlv.proxy.VoltageReading-1.csv"
@@ -78,7 +97,7 @@ def preprocess_data(
     AngularVelocityReading,
     for_training=True,
 ):
-    columns_IR = ["sampleTimeStamp.seconds", "sampleTimeStamp.microseconds", "voltage"]
+    columns_IR = ["voltage"]
     columns_velocity = [
         "sampleTimeStamp.seconds",
         "sampleTimeStamp.microseconds",
@@ -87,21 +106,11 @@ def preprocess_data(
         "angularVelocityZ",
     ]
 
-    IRLeft = load_and_clean(voltageReading1, columns_IR)
-    IRLeft.columns = [
-        "sampleTimeStamp.seconds",
-        "sampleTimeStamp.microseconds",
-        "IRLeft_voltage",
-    ]
-    IRRight = load_and_clean(voltageReading3, columns_IR)
-    IRRight.columns = [
-        "sampleTimeStamp.seconds",
-        "sampleTimeStamp.microseconds",
-        "IRRight_voltage",
-    ]
-    merge_on = ["sampleTimeStamp.seconds", "sampleTimeStamp.microseconds"]
+    merge_on = ["timestamp"]
+
     # Steering is only available during training
     if for_training:
+
         columns_steering = [
             "sampleTimeStamp.seconds",
             "sampleTimeStamp.microseconds",
@@ -111,24 +120,66 @@ def preprocess_data(
         Velocity = load_and_clean(AngularVelocityReading, columns_velocity)
         # Merge dataframes based on timestamp since dataflow is not perfectly synchronized
 
-        combined_df = merge_dataframes([IRLeft, IRRight, Steering, Velocity], merge_on)
+        combined_df = merge_dataframes([Steering, Velocity], merge_on)
     else:
         Velocity = load_and_clean(AngularVelocityReading, columns_velocity)
-        combined_df = merge_dataframes([IRLeft, IRRight, Velocity], merge_on)
+
+        combined_df = Velocity
+
+        # drop og timestamp cols
+        combined_df.drop(
+            ["sampleTimeStamp.seconds", "sampleTimeStamp.microseconds"],
+            axis=1,
+            inplace=True,
+        )
 
     # Sort dataframe by timestamp if necessary
     combined_df.sort_values(by=merge_on, inplace=True)
 
+    # save the combined data
+    combined_df.to_csv("combined_data.csv")
+
     return combined_df
 
 
-def test_model(model, new_data, imputer):
-    """Make predictions using the trained model."""
-    # missing value handling
-    new_data_imputed = imputer.transform(new_data)
+def preprocess_prediction_data(filepath, columns, scaler, imputer):
+    """Load and preprocess new data for making predictions."""
 
-    # Make predictions
-    predictions = model.predict(new_data_imputed)
+    # load the data, wwe dont need to use the clean function since the data is already cleaned
+    Velocity = pd.read_csv(filepath[0], delimiter=";", usecols=columns)
+
+    Velocity = Velocity.rename(
+        columns={
+            "sampleTimeStamp.seconds": "sampleTimeStamp.seconds",
+            "sampleTimeStamp.microseconds": "sampleTimeStamp.microseconds",
+            "angularVelocityX": "angularVelocityX",
+            "angularVelocityY": "angularVelocityY",
+            "angularVelocityZ": "angularVelocityZ",
+        }
+    )
+    # print the head of the data
+    print(Velocity.head())
+
+    #
+    # Normalize data for training/model preparation
+    features_to_scale = [
+        "angularVelocityX",
+        "angularVelocityY",
+        "angularVelocityZ",
+    ]
+
+    Velocity[features_to_scale] = scaler.transform(Velocity[features_to_scale])
+
+    # handle missing data in features
+    Velocity = imputer.transform(Velocity)
+
+    return Velocity
+
+
+def test_model(model, new_data):
+    """Make predictions using the trained model."""
+
+    predictions = model.predict(new_data)
 
     return predictions
 
@@ -144,14 +195,15 @@ def loadAllData(file_groups):
             for_training=True,
         )
         all_data_frames.append(preprocessed_data)
+
+    # save to csv
+    pd.concat(all_data_frames, ignore_index=True).to_csv("all_data.csv")
     return pd.concat(all_data_frames, ignore_index=True)
 
 
 if __name__ == "__main__":
 
     RecordingToPredict = [
-        "CSV-Files/CID-140-recording-2020-03-18_150001-selection.rec.csv/opendlv.proxy.VoltageReading-1.csv",
-        "CSV-Files/CID-140-recording-2020-03-18_150001-selection.rec.csv/opendlv.proxy.VoltageReading-3.csv",
         "CSV-Files/CID-140-recording-2020-03-18_150001-selection.rec.csv/opendlv.proxy.AngularVelocityReading-0.csv",
     ]
 
@@ -194,24 +246,29 @@ if __name__ == "__main__":
     TrainingData = loadAllData(file_paths)
 
     # Train model
-    [model, imputer] = train_model(TrainingData)
+    [model, imputer, scaler] = train_model(TrainingData)
 
     # Save model
     joblib.dump(model, "modelmultiple.pkl")
+    # Save imputer
+    joblib.dump(imputer, "imputer.pkl")
+    # Save scaler
+    joblib.dump(scaler, "scaler.pkl")
 
     # load the trained model
     model = joblib.load("modelmultiple.pkl")
+    imputer = joblib.load("imputer.pkl")
+    scaler = joblib.load("scaler.pkl")
 
     # Load and preprocess new data
-    PredictionData = preprocess_data(
-        voltageReading1=RecordingToPredict[0],
-        voltageReading3=RecordingToPredict[1],
-        GroundSteering=None,
-        AngularVelocityReading=RecordingToPredict[2],
-        for_training=False,
+    PredictionData = preprocess_prediction_data(
+        RecordingToPredict,
+        columns=["angularVelocityX", "angularVelocityY", "angularVelocityZ"],
+        scaler=scaler,
+        imputer=imputer,
     )
 
-    predictions = test_model(model, PredictionData, imputer)
+    predictions = test_model(model, PredictionData)
 
     # save the predictions
     pd.DataFrame(predictions).to_csv("predictions.csv")
@@ -228,10 +285,44 @@ if __name__ == "__main__":
         ],
     )
 
-    plt.plot(Steering_new["groundSteering"], label="Original Steering")
-    plt.plot(predictions, label="Predicted Steering")
+    # we only care about the predictions where we have steering data so only plot the predictions where we have steering data
+    predictions = pd.DataFrame(predictions)
+    predictions["timestamp"] = Steering_new["timestamp"]
+    predictions = predictions.dropna()
+
+    # plt.plot(
+    #     Steering_new["timestamp"], Steering_new["groundSteering"], label="Steering"
+    # )
+    # plt.plot(predictions["timestamp"], predictions[0], label="Predictions")
+
+    # plot where the predictions is withing +-25% of the steering data so upper range is steering * 1.25 and lower range is steering * 0.75
+    plt.plot(
+        Steering_new["timestamp"],
+        Steering_new["groundSteering"],
+        label="Steering",
+        color="blue",
+    )
+    plt.plot(
+        predictions["timestamp"],
+        predictions[0],
+        label="Predictions",
+        color="red",
+    )
+
+    plt.fill_between(
+        Steering_new["timestamp"],
+        Steering_new["groundSteering"] * 0.75,
+        Steering_new["groundSteering"] * 1.25,
+        color="gray",
+        alpha=0.5,
+    )
+
+    plt.xlabel("Time")
+
+    plt.ylabel("Steering")
+
     plt.legend()
 
-    ## calculate whether the predicted steering is withing a +- 25% range, but only for when prediction and steering are both available
-
     plt.show()
+
+    print("done")
