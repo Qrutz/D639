db9b2f09da0c252c899aeeccca72f24c931f02bf
Johan Sandgren <gussanjoba@student.gu.se>
2024-05-13 16:58:47 +0200

    Refactor plot.py to improve readability and add group name overlay

--- src/plot.py
+++ src/plot.py
@@ -8,17 +8,43 @@ data = pd.read_csv("steeringAngles.csv")
 data["Timestamp"] = (data["Timestamp"] - data["Timestamp"].min()) / 1e6
 
 # Plotting
-plt.figure(figsize=(10, 5))
-plt.plot(data["Timestamp"], data[" SteeringAngle"], label="Steering Angle", marker="o")
+
 plt.plot(
-    data["Timestamp"], data[" OriginalSteering"], label="Original Steering", marker="x"
+    data["Timestamp"],
+    data[" OriginalSteering"],
+    label="GroundSteeringRequest.groundSteering",
+    linestyle="--",
+    color="red",
 )
+plt.plot(
+    data["Timestamp"],
+    data[" SteeringAngle"],
+    label="Computed steering angle",
+    linestyle="-",
+    color="blue",
+)
+
 
 # Adding titles and labels
 plt.title("Steering Angle Comparison Over Time")
-plt.xlabel("Time (seconds from start)")
+plt.xlabel("sampleTime in microseconds")
 plt.ylabel("Steering Angle")
+
 plt.legend()
+
+
+# Adding text overlay for group name
+plt.text(
+    0.5,
+    0.5,
+    "Group 16",
+    fontsize=24,
+    va="center",
+    ha="center",
+    transform=plt.gca().transAxes,
+    color="black",
+    alpha=1,
+)
 
 # Display the plot
 plt.grid(True)
