86d7f33fe67c525a173ddc30efc11fb5e1c20bd4
Johan Sandgren <gussanjoba@student.gu.se>
2024-05-16 15:40:23 +0200

    Uses ml for counterclockwise

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,31 @@
 int32_t main(int32_t argc, char **argv)
 {
     int32_t retCode{1};
+
+    float steeringWheelAngle = 0.0f;
+    float MLSteeringAngle = 0.0f;
+    int direction = 0; // -1 for clockwise, 1 for counter-clockwise
+
+    // For TESTING STUFF
+    int totalEntries = 0;
+    int totalWithinRange = 0;
+
+    // Open a file for writing our steering angles
+    std::ofstream outputFile("../steeringAngles.csv", std::ios::out | std::ios::app);
+
+    // Write headers if the file is new or empty
+    if (outputFile.tellp() == 0)
+    {
+        outputFile << "Timestamp, SteeringAngle, OriginalSteering\n";
+    }
+    else
+    {
+        // If the file is not empty, we need to clear it
+        outputFile.close();
+        outputFile.open("../steeringAngles.csv", std::ios::out | std::ios::trunc);
+        outputFile << "Timestamp, SteeringAngle, OriginalSteering\n";
+    }
+
     // Parse the command line parameters as we require the user to specify some mandatory information on startup.
     auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
     if ((0 == commandlineArguments.count("cid")) ||
@@ -59,7 +84,6 @@ int32_t main(int32_t argc, char **argv)
         const uint32_t HEIGHT{static_cast<uint32_t>(std::stoi(commandlineArguments["height"]))};
         const bool VERBOSE{commandlineArguments.count("verbose") != 0};
 
-
         // Attach to the shared memory.
         std::unique_ptr<cluon::SharedMemory> sharedMemory{new cluon::SharedMemory{NAME}};
         if (sharedMemory && sharedMemory->valid())
@@ -78,43 +102,46 @@ int32_t main(int32_t argc, char **argv)
                 // https://github.com/chrberger/libcluon/blob/master/libcluon/testsuites/TestEnvelopeConverter.cpp#L31-L40
                 std::lock_guard<std::mutex> lck(gsrMutex);
                 gsr = cluon::extractMessage<opendlv::proxy::GroundSteeringRequest>(std::move(env));
-                //std::cout << "lambda: groundSteering = " << gsr.groundSteering() << std::endl;
+                // std::cout << "lambda: groundSteering = " << gsr.groundSteering() << std::endl;
             };
 
             od4.dataTrigger(opendlv::proxy::GroundSteeringRequest::ID(), onGroundSteeringRequest);
 
-            //cv::namedWindow("Combined Color tracking", cv::WINDOW_AUTOSIZE);
-            //cv::createTrackbar("maxContourArea", "Combined Color tracking", &maxContourArea, 2500);
-            //cv::createTrackbar("minContourArea", "Combined Color tracking", &minContourArea, 2500);
+            // Revieve incoming steering commands from the ML model, and update the steering angle
+            SteeringCommand sc;
+            auto onPythonMessage = [&sc, &MLSteeringAngle](cluon::data::Envelope &&env)
+            {
+                sc = cluon::extractMessage<SteeringCommand>(std::move(env));
+                MLSteeringAngle = sc.steeringAngle();
+            };
 
+            od4.dataTrigger(SteeringCommand::ID(), onPythonMessage);
+
+            // cv::namedWindow("Combined Color tracking", cv::WINDOW_AUTOSIZE);
+            // cv::createTrackbar("maxContourArea", "Combined Color tracking", &maxContourArea, 2500);
+            // cv::createTrackbar("minContourArea", "Combined Color tracking", &minContourArea, 2500);
 
             DirectionCalculator directionCalculator;
             AngleCalculator angleCalculator;
 
             // Car position on the X axis
-            //const int carPositionX = 320;
+            // const int carPositionX = 320;
 
-            float steeringWheelAngle = 0.0f;
             const float maxSteering = 0.3f;
             const float minSteering = -0.3f;
             int frameCount = 0;
             int direction = 0; // -1 for clockwise, 1 for counter-clockwise
 
-            // For TESTING STUFF
-            int totalEntries = 0;
-            int totalWithinRange = 0;
-
-
             // Endless loop; end the program by pressing Ctrl-C.
             while (od4.isRunning())
             {
 
-                frameCount++;  // Count the number of frames processed.
+                frameCount++; // Count the number of frames processed.
 
                 // OpenCV data structure to hold an image.
-                
+
                 cv::Mat img;
-                cv::Mat hsvImg;    // HSV Image
+                cv::Mat hsvImg; // HSV Image
                 cv::Mat finalThresh;
 
                 // Wait for a notification of a new frame.
@@ -129,22 +156,25 @@ int32_t main(int32_t argc, char **argv)
                 }
 
                 // We start off by detecting if the track is moving in a clockwise or counter-clockwise direction.
-                if(frameCount % 15 == 0 || frameCount < 10){
+                if (frameCount % 15 == 0 || frameCount < 10)
+                {
                     direction = directionCalculator.CalculateDirection(img, direction);
-                    if(direction == -1){
+                    if (direction == -1)
+                    {
                         std::cout << "Direction: Clockwise" << std::endl;
                     }
-                    else if(direction == 1){
+                    else if (direction == 1)
+                    {
                         std::cout << "Direction: Counter-Clockwise" << std::endl;
                     }
-                    else{
+                    else
+                    {
                         std::cout << "Direction: No direction" << std::endl;
                     }
                 }
                 // Crop bottom half. Only bottom 50% part will be used for processing and contour tracking.
                 cv::Rect roi(0, img.rows / 2, img.cols, img.rows / 2);
                 cv::Mat croppedImg = img(roi);
-
 
                 // inRange filters out blue colors. Use gaussian blur to smooth out image, and morphological operations
                 // Erode makes objects smaller but fills in the holes. Dilate does the opposite, so if you combine them
@@ -157,86 +187,93 @@ int32_t main(int32_t argc, char **argv)
                 yellowThreshImg = noiseRemover.RemoveNoise(yellowThreshImg);
                 blueThreshImg = noiseRemover.RemoveNoise(blueThreshImg);
 
-                //cv::Mat yellowContourOutput = contourFinder.FindContours(yellowThreshImg, img, minContourArea, maxContourArea);
-                //cv::Mat blueContourOutput = contourFinder.FindContours(blueThreshImg, img, minContourArea, maxContourArea);
+                // cv::Mat yellowContourOutput = contourFinder.FindContours(yellowThreshImg, img, minContourArea, maxContourArea);
+                // cv::Mat blueContourOutput = contourFinder.FindContours(blueThreshImg, img, minContourArea, maxContourArea);
 
                 // If clockwise map, blue cones on left side, yellow cones on right side.
                 // If counter-clockwise map, blue cones on right side, yellow cones on left side.
-                if (direction != 0) {
+                if (direction == 1)
+                {
+                    // use ml steering angle
+                    steeringWheelAngle = MLSteeringAngle;
+                }
+                else
+
+                {
                     bool isClockwise = (direction == -1);
                     steeringWheelAngle = angleCalculator.CalculateSteeringAngle(yellowThreshImg, blueThreshImg, steeringWheelAngle, isClockwise, maxSteering, minSteering);
                 }
 
-
-                //cv::bitwise_or(blueContourOutput, yellowContourOutput, finalThresh);
+                // cv::bitwise_or(blueContourOutput, yellowContourOutput, finalThresh);
 
                 // Now we can combine the contours with the original picture. The reason for doing this is to eliminate the noise
                 // in the top part of the picture, and the contour processing is only done on 50% of the original image, which should
                 // increase performance.
-                //cv::Mat finalOutput;
-                //cv::addWeighted(img, 1.0, finalThresh, 1.0, 0.0, finalOutput);
+                // cv::Mat finalOutput;
+                // cv::addWeighted(img, 1.0, finalThresh, 1.0, 0.0, finalOutput);
 
                 // TODO: Here, you can add some code to check the sampleTimePoint when the current frame was captured.
+
+                std::pair<bool, cluon::data::TimeStamp> ts = sharedMemory->getTimeStamp();
+
+                int64_t sampleTimePoint = cluon::time::toMicroseconds(ts.second);
+                std::string ts_string = std::to_string(sampleTimePoint);
+
                 sharedMemory->unlock();
 
-        {
-            std::lock_guard<std::mutex> lck(gsrMutex);
-            float actualSteering = gsr.groundSteering();  // Obtain the ground truth steering angle
-            float epsilon = 0.001f;  // Define a small epsilon value
-
-            // Check if the ground truth steering angle is not effectively zero
-            if (fabs(actualSteering) > epsilon)  {
-                // group_XY;sampleTimeStamp in microseconds;steeringWheelAngle
-                std::cout << "Actualy Steering : " << actualSteering << std::endl;
-                std::cout << "Calculated Angle : " << steeringWheelAngle << std::endl;
-
-                // Calculate the lower and upper bounds for the allowed steering range
-                float lowerBound = std::min(actualSteering * 0.75f, actualSteering * 1.25f);
-                float upperBound = std::max(actualSteering * 0.75f, actualSteering * 1.25f);
-
-
-                // Check if the steeringWheelAngle is within the calculated range
-                bool isWithinRange = (steeringWheelAngle >= lowerBound) && (steeringWheelAngle <= upperBound);
-
-                // If the steering wheel angle is within the acceptable range
-                if (isWithinRange) {
-                    totalWithinRange++;
-                    std::cout << "Total within range: " << totalWithinRange << " / " << totalEntries << std::endl;
-                }
-
-                totalEntries++; // Increment the total number of entries checked
-            } else {
-                // Output when skipping frames where the ground truth steering angle is zero
-                std::cout << "Actualy Steering : " << actualSteering << std::endl;
-                std::cout << "Calculated Angle : " << steeringWheelAngle << std::endl;
-                std::cout << "Total within range: " << totalWithinRange << " / " << totalEntries << std::endl;
-                if (totalEntries > 0) {
-                    std::cout << "Percentage within range: " << (static_cast<float>(totalWithinRange) / totalEntries) * 100 << "%" << std::endl;
-                    } 
-                else {
-                    std::cout << "No entries to evaluate." << std::endl;
-                    }
-                }
-        }
-
-
                 // If you want to access the latest received ground steering, don't forget to lock the mutex:
+                {
+                    std::lock_guard<std::mutex> lck(gsrMutex);
+                    float actualSteering = gsr.groundSteering();
+                    // group_XY;sampleTimeStamp in microseconds;steeringWheelAngle
+
+                    float lowerBound = std::min(actualSteering * 0.75f, actualSteering * 1.25f);
+                    float upperBound = std::max(actualSteering * 0.75f, actualSteering * 1.25f);
+
+                    bool isWithinRange = (steeringWheelAngle >= lowerBound) && (steeringWheelAngle <= upperBound);
+                    std::cout << "group_16;" << ts_string << ";" << steeringWheelAngle << std::endl;
+                    if (actualSteering != 0.0)
+                    {
+
+                        if (isWithinRange)
+                        {
+                            totalWithinRange++;
+                            // print within range frames
+                            std::cout << "total frames within range: " << totalWithinRange << " frames:" << totalEntries << "\n";
+                        }
+                        totalEntries++;
+                    }
+
+                    // write to file
+                    outputFile << ts_string << "," << steeringWheelAngle << "," << gsr.groundSteering() << "\n";
+
+                    // check if the steering angle is within +-25% of the actual steering
+                }
 
                 // Display image on your screen.
                 if (VERBOSE)
                 {
-
-                    //cv::imshow(sharedMemory->name().c_str(), img);
-                    //cv::imshow("ResultImg", img);
-                    //cv::imshow("Blue", blueContourOutput);
-                    //cv::imshow("Yellow", yellowContourOutput);
-                    //cv::imshow("Combined Color tracking", finalOutput);
+                    // cv::imshow(sharedMemory->name().c_str(), img);
+                    // cv::imshow("ResultImg", img);
+                    // cv::imshow("Blue", blueContourOutput);
+                    // cv::imshow("Yellow", yellowContourOutput);
+                    // cv::imshow("Combined Color tracking", finalOutput);
 
                     cv::waitKey(1);
                 }
             }
         }
         retCode = 0;
+
+        std::cout << "Total entries: " << totalEntries << std::endl;
+        std::cout << "Total within range: " << totalWithinRange << std::endl;
+        // print percentage of frames within range
+        float percentageWithinRange = (static_cast<float>(totalWithinRange) / totalEntries) * 100.0f;
+        std::cout << "Percentage of frames within range: " << percentageWithinRange << "%" << std::endl;
     }
+
+    // Close the file
+    outputFile.close();
+
     return retCode;
 }
